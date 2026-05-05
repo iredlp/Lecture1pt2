0124c3891e5e2a3bf9294e34fbc6b5e25a251ffd
iredlp <s311559@studenti.polito.it>
2026-05-05 17:36:59 +0200

    Ho aggiunto una pagina duplicata con più commenti Ho aggiunto i Counter, fatto esempi sui DICT e DIZIONARI Aggiunto la pag gestore Ordini Mio Ho solo aggiunto dei commenti nelle pagine di controller, view ecc Ho aggiunto le utili versioni de controller e migliorato l'interfaccia grafica. Ho cambiato il nome di una funzione richiamata poi nel controller facendola diventaere get_riepilogo Aggiunto collegamento base con bd con dbver

---
+++ dao/dbConnect.py
@@ -0,0 +1,17 @@
+import mysql
+
+
+class dbConnect:
+    def getConnection(self):
+        try:
+            cnx=mysql.connector.connect(
+                user="root",
+                password="rootroot",
+                host="127.0.0.1",
+                database="sw_gestonale"
+            )
+            return cnx
+        except mysql.connector.Error as err:
+            print("Non riesco a collegarmi al  database")
+            print(err)
+            return None
